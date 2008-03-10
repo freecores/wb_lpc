b8d80b4cafc009de44ca3897a4ea3475ff14f662
hharte <hharte@90b43792-ffaf-4799-9aa5-e55e815950c2>
2008-03-10 14:08:13 +0000

    Add Serial IRQ Support

--- rtl/verilog/serirq_defines.v
+++ rtl/verilog/serirq_defines.v
@@ -1,5 +1,6 @@
 //////////////////////////////////////////////////////////////////////
 ////                                                              ////
+////  $Id: serirq_defines.v,v 1.1 2008-03-10 14:08:13 hharte Exp $
 ////  wb_lpc_defines.v                                            ////
 ////                                                              ////
 ////  This file is part of the Wishbone LPC Bridge project        ////
@@ -35,48 +36,18 @@
 ////                                                              ////
 //////////////////////////////////////////////////////////////////////
 
-// Wishbone LPC Master/Slave Interface Definitions
+// Wishbone SERIRQ Host/Slave Interface Definitions
+`define SERIRQ_ST_IDLE    13'h000             // SERIRQ Idle state
+`define SERIRQ_ST_START   13'h001             // SERIRQ Start state
+`define SERIRQ_ST_START_R 13'h002             // SERIRQ Start state
+`define SERIRQ_ST_START_T 13'h004             // SERIRQ Start state
+`define SERIRQ_ST_IRQ     13'h008             // SERIRQ IRQ Frame State
+`define SERIRQ_ST_IRQ_R   13'h010             // SERIRQ IRQ Frame State
+`define SERIRQ_ST_IRQ_T   13'h020             // SERIRQ IRQ Frame State
+`define SERIRQ_ST_STOP    13'h040             // SERIRQ Stop State
+`define SERIRQ_ST_STOP_R  13'h080             // SERIRQ Stop State
+`define SERIRQ_ST_STOP_T  13'h100             // SERIRQ Stop State
+`define SERIRQ_ST_WAIT_STOP 13'h200
 
-`define LPC_START       4'b0000
-`define LPC_STOP        4'b1111
-`define LPC_FW_READ     4'b1101
-`define LPC_FW_WRITE    4'b1110
-
-`define LPC_SYNC_READY  4'b0000             // LPC Sync Ready
-`define LPC_SYNC_SWAIT  4'b0101             // LPC Sync Short Wait (up to 8 cycles)
-`define LPC_SYNC_LWAIT  4'b0110             // LPC Sync Long Wait (no limit)
-`define LPC_SYNC_MORE   4'b1001             // LPC Sync Ready More (DMA only)
-`define LPC_SYNC_ERROR  4'b1010             // LPC Sync Error
-
-`define LPC_ST_IDLE     13'h000             // LPC Idle state
-`define LPC_ST_START    13'h001             // LPC Start state
-`define LPC_ST_CYCTYP   13'h002             // LPC Cycle Type State
-`define LPC_ST_ADDR     13'h004             // LPC Address state (4 cycles)
-`define LPC_ST_CHAN     13'h008             // LPC Address state (4 cycles)
-`define LPC_ST_SIZE     13'h010             // LPC Address state (4 cycles)
-`define LPC_ST_H_DATA   13'h020             // LPC Host Data state (2 cycles)
-`define LPC_ST_P_DATA   13'h040             // LPC Peripheral Data state (2 cycles)
-`define LPC_ST_H_TAR1   13'h080             // LPC Host Turnaround 1 (Drive LAD 4'hF)
-`define LPC_ST_H_TAR2   13'h100             // LPC Host Turnaround 2 (Float LAD)
-`define LPC_ST_P_TAR1   13'h200             // LPC Peripheral Turnaround 1 (Drive LAD = 4'hF)
-`define LPC_ST_P_TAR2   13'h400             // LPC Peripheral Turnaround 2 (Float LAD)
-`define LPC_ST_WB_RETIRE 13'h400            // Retire Wishbone transfer (Host only), ends WB cycle.
-`define LPC_ST_SYNC     13'h800             // LPC Sync State (may be multiple cycles for wait-states)
-`define LPC_ST_P_WAIT1  13'h1000            // LPC Sync State (may be multiple cycles for wait-states)
-
-
-`define WB_SEL_BYTE     4'b0001             // Byte Transfer
-`define WB_SEL_SHORT    4'b0011             // Short Transfer
-`define WB_SEL_WORD     4'b1111             // Word Transfer
-
-`define WB_TGA_MEM      2'b00               // Memory Cycle
-`define WB_TGA_IO       2'b01               // I/O Cycle
-`define WB_TGA_FW       2'b10               // Firmware Cycle
-`define WB_TGA_DMA      2'b11               // DMA Cycle
-
-// LDRQ States
-
-`define LDRQ_ST_IDLE    4'h0
-`define LDRQ_ST_ADDR    4'h1
-`define LDRQ_ST_ACT     4'h2
-`define LDRQ_ST_DONE    4'h4
+`define SERIRQ_MODE_CONTINUOUS 1'b0           // Serirq "Continuous Mode"
+`define SERIRQ_MODE_QUIET 1'b1                // Serirq "Quiet Mode"
